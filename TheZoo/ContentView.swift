6bd9bc45ec39bbe638e9094f9b22fe76e5bfaaf9
Mingyi Jin <m.jin-1@student.tudelft.nl>
2025-04-08 03:37:45 +0200

    Initial Commit

---
+++ TheZoo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TheZoo
+//
+//  Created by 　金明毅 on 2025/4/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheZoo/TheZooApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheZooApp.swift
+//  TheZoo
+//
+//  Created by 　金明毅 on 2025/4/8.
+//
+
+import SwiftUI
+
+@main
+struct TheZooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
